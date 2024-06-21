ad748a4aa4949fe51523cb5120a7c4c2cbaff20e
kalpanachamling99 <kalpanachamling99@gmail.com>
2024-06-21 14:39:40 +0545

    fix ui of nutriswish

--- js/ScriptIngredient.js
+++ js/ScriptIngredient.js
@@ -18,37 +18,59 @@ document.addEventListener("DOMContentLoaded", function () {    // Product Slider
         },
       }
     );
-  
+
     keyIngredientsSliderMobile.mount();
-  
-    var keyIngredientsSliderDesktop = new Splide(
-      "#key-ingredients-slider-desktop",
-      {
-        arrows: false,
-        pagination: false,
-        perPage: 1,
-        arrows: true,
-        cover: false,
-        gap: "2rem",
-        mediaQuery: "min",
-        rewind: false,
-        breakpoints: {
-          1280: {
-            perPage: 4,
-          },
-          1024: {
-            perPage: 3,
-          },
-          768: {
-            perPage: 2,
-          },
-          576: {
-            perPage: 2,
-          },
+
+    // var keyIngredientsSliderDesktop = new Splide(
+    //   "#key-ingredients-slider-desktop",
+    //   {
+    //     arrows: false,
+    //     pagination: false,
+    //     perPage: 1,
+    //     arrows: true,
+    //     cover: false,
+    //     gap: "2rem",
+    //     mediaQuery: "min",
+    //     rewind: false,
+    //     breakpoints: {
+    //       1280: {
+    //         perPage: 4,
+    //       },
+    //       1024: {
+    //         perPage: 3,
+    //       },
+    //       768: {
+    //         perPage: 2,
+    //       },
+    //       576: {
+    //         perPage: 2,
+    //       },
+    //     },
+    //   }
+    // );
+    var keyIngredientsSliderDesktop = new Splide("#key-ingredients-slider-desktop", {
+      arrows: false,
+      pagination: false,
+      perPage: 1,
+      arrows: true,
+      cover: false,
+      gap: "2rem",
+      mediaQuery: "min",
+      rewind: false,
+      breakpoints: {
+        1280: {
+          perPage: 3,
+          width: "75%"
         },
-      }
-    );
-  
+        768: {
+          perPage: 3,
+        },
+        576: {
+          perPage: 2,
+        },
+      },
+    });
+
     keyIngredientsSliderDesktop.mount();
 
     const keyItems = gsap.utils.toArray(".ingredients .item");
@@ -89,5 +111,3 @@ document.addEventListener("DOMContentLoaded", function () {    // Product Slider
       );
     });
   });
-
-  
