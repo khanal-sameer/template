19c623415a41902319318729d53cc3616c281bc0
kalpanachamling99 <kalpanachamling99@gmail.com>
2024-07-05 18:11:49 +0545

    design product description,feature,trusted partner,benefits and tab menu section

--- js/ScriptDemoProductPage.js
+++ js/ScriptDemoProductPage.js
@@ -23,4 +23,49 @@ document.addEventListener("DOMContentLoaded", function () {
   previewSlider.sync(thumbnailsSlider);
   previewSlider.mount();
   thumbnailsSlider.mount();
+
+  var trustedBy = new Splide("#trusted-by-slider", {
+    autoWidth: true,
+      arrows: false,
+      pagination: false,
+      type: "loop",
+      perPage: 7,
+      autoplay: true,
+      interval: 2000,
+      cover: false,
+      gap: "4rem",
+      breakpoints: {
+        1024: {
+          perPage: 6,
+          gap: "4rem",
+        },
+        576: {
+          perPage: 7,
+        },
+      },
+  });
+  trustedBy.mount();
+
+  var testimonialSlider = new Splide("#testimonial-slider",{
+    arrows: false,
+    pagination: false,
+    perPage: 1,
+    cover: false,
+    gap: "20px",
+    padding: {
+      right: "50px"
+    },
+    mediaQuery: "min",
+    rewind: false,
+    breakpoints: {
+      768: {
+        perPage: 2,
+        arrows: true,
+        padding: {
+        right: "0px"
+      },
+      },
+    },
+  });
+  testimonialSlider.mount();
 });
