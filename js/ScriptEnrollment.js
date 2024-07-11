a4a7d93bf452e809b58396449af71e3ec47fec76
khanal-sameer <57002785+Sameer725@users.noreply.github.com>
2024-07-11 15:06:23 +0545

    fix: enrollments for draft

--- js/ScriptEnrollment.js
+++ js/ScriptEnrollment.js
@@ -1,75 +1,101 @@
-document.addEventListener('DOMContentLoaded', async function () {
+document.addEventListener("DOMContentLoaded", async function () {
+  function extractCookie(...keys) {
+    return document.cookie
+      .split(";")
+      .filter((item) => keys.some((key) => item.includes(key)))
+      .reduce((acc, item) => {
+        const [key, value] = item.trim().split("=");
+        acc[key] = value;
 
-    function extractCookie(...keys) {
-      return document.cookie.split(";").filter(item => keys.some(key => item.includes(key))).reduce((acc, item) => {
-        const [key, value] = item.trim().split("=")
-        acc[key] = value
-  
-        return acc
-      }, {})
+        return acc;
+      }, {});
+  }
+
+  const cookie = extractCookie("country", "locale");
+  const country = window.country || cookie.country || "us";
+  const locale = window.locale || cookie.locale || "en";
+  const selector = '[data-product^="product_"]';
+
+  const container = document.getElementById("enroll-pack");
+  const containers = container.querySelectorAll(selector);
+
+  const extractId = (id = "") => id.split("_").pop();
+
+  const fetchEnrollmentDetail = async (product_id) => {
+    if (!product_id) return null;
+
+    const host = "https://app.neumi.com";
+    const url = new URL(
+      `${host}/api/v1/enrollment_packs/${product_id}?country_iso=${country}&language_iso=${locale}`
+    );
+
+    try {
+      const res = await fetch(url);
+      const data = await res.json();
+
+      return data;
+    } catch {
+      return null;
     }
-  
-    const cookie = extractCookie('country', "locale");
-    const country = window.country || cookie.country || 'us'
-    const locale = window.locale || cookie.locale || 'en'
-    const selector = '[data-product^="product_"]'
+  };
 
-  
-    const container = document.getElementById("enroll-pack")
-    const containers = container.querySelectorAll(selector)
+  const promises = [...containers].map(async (element) => {
+    const productId = element.getAttribute("data-product");
+    const id = extractId(productId);
 
-    const extractId = (id="") => id.split("_").pop()
-    
-    const fetchEnrollmentDetail = async (product_id) => {
-  
-      if (!product_id) return null
-  
-      const host = "https://app.neumi.com"
-      const url = new URL(`${host}/api/v1/enrollment_packs/${product_id}?country_iso=${country}&language_iso=${locale}`)
-  
-      try {
-        const res = await fetch(url)
-        const data = await res.json()
+    const data = (await fetchEnrollmentDetail(id)) || {};
+    const {
+      display_price: p,
+      image_url,
+      title,
+      membership_products,
+      subscription_products,
+    } = data;
 
-        return data
-      }
-      catch {
-        return null
-      }
+    const sub = (subscription_products || [])[0];
+    const one_time = (membership_products || [])[0];
+
+    const { title: subTitle = "", active: subActive } = sub || {};
+    let { title: oneTimeTitle = "", active: oneTimeActive } = one_time || {};
+
+    oneTimeTitle =
+      subTitle && oneTimeTitle ? oneTimeTitle + " + " : oneTimeTitle;
+
+    return {
+      id,
+      title,
+      desc: oneTimeTitle + subTitle,
+      title,
+      img: image_url,
+      hide: !subActive && !oneTimeActive,
+      element,
+      price: p,
+    };
+  });
+
+  const enrollments = await Promise.all(promises);
+  const hasEnrollments = enrollments.some((item) => !item.hide);
+
+  enrollments.forEach(({ desc, element, hide, id, img, title, price }) => {
+    const enroll = container.querySelector(`[data-product="enrollment_${id}"]`);
+
+    if (hide) {
+      element.remove();
+      enroll.remove();
+      return;
     }
 
-    containers.forEach(async (element, index)=> {
-        const productId = element.getAttribute("data-product")
-        const id = extractId(productId);
-    
-        const data = await fetchEnrollmentDetail(id) || {}
-        const { display_price: p, image_url, title, membership_products,subscription_products } = data;
+    const imageElement = element.querySelector(".p-image");
+    imageElement.src = img;
 
-        if(!p) return ;
+    const descElement = enroll.querySelector(".p-desc");
+    const titleElement = enroll.querySelector(".p-title");
+    const priceElement = enroll.querySelector(".p-price");
 
-        const sub =  subscription_products?.map(i=>i.title).join(",")
+    titleElement.textContent = title;
+    priceElement.textContent = price;
+    descElement.textContent = desc;
+  });
 
-        let one_time = membership_products?.map(i=>i.title).join(",")
-        one_time = (sub && one_time) ? one_time + " + " : one_time
-
-        const des = one_time + sub
-
-
-
-        const img = element.querySelector('.p-image')
-        img.src = image_url
-
-        const enroll = container.querySelector(`[data-product="enrollment_${id}"]`)
-
-        const desc = enroll.querySelector('.p-desc')
-        const name = enroll.querySelector('.p-title')
-        const price = enroll.querySelector('.p-price')
-
-        name.textContent = title;
-        price.textContent = p
-        desc.textContent = des
-
-
-        if(containers.length === index + 1) container.classList.remove("is-hide")
-    })
-  })
+  if (hasEnrollments) container.classList.remove("is-hide");
+});
