6026755dfc9cc3c75bd125c82d59ff6980f32490
kalpanachamling99 <kalpanachamling99@gmail.com>
2024-09-04 16:09:42 +0545

    refactor stylesheet and script of demo product page

--- js/demo_company_product/ScriptDemoCompanyProduct.js
+++ js/demo_company_product/ScriptDemoCompanyProduct.js
@@ -25,57 +25,6 @@ function productSlider(){
   previewSlider.mount();
   thumbnailsSlider.mount();
 }
-function testimonialSlider(){
-  var slider = new Splide("#testimonial-slider",{
-    arrows: false,
-    pagination: false,
-    perPage: 1,
-    cover: false,
-    gap: "20px",
-    padding: {
-      right: "50px"
-    },
-    mediaQuery: "min",
-    rewind: false,
-    breakpoints: {
-      576: {
-        perPage: 2,
-        arrows: true,
-        padding: {
-          right: "0px"
-        },
-      },
-    },
-  });
-  slider.mount();
-}
-function trustedBySlider(){
-  var slider = new Splide("#trusted-by-slider", {
-    arrows: false,
-    pagination: false,
-    type: "loop",
-    perPage: 3,
-    autoplay: true,
-    interval: 2000,
-    cover: false,
-    gap: 30,
-    mediaQuery: "min",
-    rewind: true,
-    breakpoints: {
-      1350:{
-        perPage: 7,
-        gap: 40,
-      },
-      1024: {
-        perPage: 5,
-      },
-      768: {
-        perPage: 4,
-      },
-    },
-  });
-  slider.mount();
-}
 function subscribe(){
   $(document).on('click','#subscribe',function(){
     $('.subscribe-card').addClass('active');
@@ -104,14 +53,7 @@ function quantity(){
     quantitySelector.val(value);
   })
 }
-function accordion(){
-  $(document).on("click",".accordion-header",function(){
-    $(".accordion-content").not($(this).next()).slideUp();
-    $(".accordion-item").not($(this).parent()).removeClass("active");
-    $(this).next(".accordion-content").slideToggle();
-    $(this).parent(".accordion-item").toggleClass("active");
-  });
-}
+
 function tabMenu(){
   $(document).on("click", ".cp-tab-menu li", function(e){
     e.preventDefault();
@@ -130,11 +72,9 @@ function tabMenu(){
   })
 }
 document.addEventListener("DOMContentLoaded", function () {
+  console.log('hello')
   productSlider();
-  testimonialSlider();
-  trustedBySlider();
   subscribe();
   quantity();
-  accordion();
   tabMenu();
 });
